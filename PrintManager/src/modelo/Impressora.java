177fb35229ea7b6336afded9cf56576a21c50195
Guilherme Wolner <gwdm@a.recife.ifpe.edu.br>
2020-06-02 20:21:08 -0300

    Rename project

--- PrintManager/src/modelo/Impressora.java
+++ PrintManager/src/modelo/Impressora.java
@@ -3,17 +3,17 @@ package modelo;
 public class Impressora {
 
 	
-	private static Impressora printer; //Tipo static para poder ser acessado pleo método tambem static.
+	private static Impressora impressora; //Tipo static para poder ser acessado pleo método tambem static.
 	
 	private Impressora() {
 		//Construtor privado para evitar que seja instanciado por qualquer um.
 	}
 	
 	public static Impressora getInstance() { //Método static para acessar instancia que contem o objeto.
-		if(printer == null) {
-			printer = new Impressora();
+		if(impressora == null) {
+			impressora = new Impressora();
 		}
 		
-		return printer;
+		return impressora;
 	}
 }
